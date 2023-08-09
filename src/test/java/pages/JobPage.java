de6f24f9d36c4ed5d5b8ff196cebda2aac0be339
MoncefBelg <moncefbe@hotmail.com>
2023-08-10 00:25:47 +0100

    first commit

---
+++ src/test/java/pages/JobPage.java
@@ -0,0 +1,30 @@
+package pages;
+
+import Utils.CommonMethods;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.CommonMethods;
+
+import static Utils.CommonMethods.driver;
+
+public class JobPage extends CommonMethods {
+    @FindBy(id="btnAdd")
+    public WebElement addButton;
+
+    @FindBy(id="jobTitle_jobTitle")
+    public WebElement jobTitleF;
+    @FindBy(id="jobTitle_jobDescription")
+    public WebElement jobDescF;
+    @FindBy(id="jobTitle_note")
+    public WebElement jobNoteF;
+
+    @FindBy(id="btnSave")
+    public WebElement jobSvBtn;
+
+
+
+   public JobPage(){
+       PageFactory.initElements(driver,this);
+    }
+}
